60b24db2e4f6549dfb65b6946c187da51f54e345
daniel-andrews1994 <38686400+daniel-andrews1994@users.noreply.github.com>
2019-06-27 13:46:58 +0100

    wip

--- it/v1/endpoints/CreateBFLossControllerISpec.scala
+++ it/v1/endpoints/CreateBFLossControllerISpec.scala
@@ -22,14 +22,15 @@ import play.api.http.Status
 import play.api.libs.json.{JsValue, Json}
 import play.api.libs.ws.{WSRequest, WSResponse}
 import support.IntegrationBaseSpec
+import v1.models.errors.{DownstreamError, NinoFormatError, NotFoundError, RuleTaxYearRangeExceededError, RuleTypeOfLossUnsupported}
 import v1.stubs.{AuditStub, AuthStub, DesStub, MtdIdLookupStub}
 
 class CreateBFLossControllerISpec extends IntegrationBaseSpec {
 
   private trait Test {
 
-    val nino    =  "AA123456A"
-    val lossId  = "AAZZ1234567890a"
+    val nino = "AA123456A"
+    val lossId = "AAZZ1234567890a"
     val correlationId = "X-123"
 
     val requestBody: JsValue = Json.parse(
@@ -39,6 +40,13 @@ class CreateBFLossControllerISpec extends IntegrationBaseSpec {
         |    "typeOfLoss": "self-employment",
         |    "taxYear": "2019-20",
         |    "lossAmount": 256.78
+        |}
+      """.stripMargin)
+
+    val responseBody: JsValue = Json.parse(
+      """
+        |{
+        |    "id": "dfgdfgdfg"
         |}
       """.stripMargin)
 
@@ -80,7 +88,64 @@ class CreateBFLossControllerISpec extends IntegrationBaseSpec {
 
         val response: WSResponse = await(request().post(requestBody))
         response.status shouldBe Status.CREATED
+        response.json shouldBe responseBody
+
       }
     }
+
+
+    /*    "return a 404 status code" when {
+
+      "any valid request is made but no income source has been found" in new CreateBFLossControllerTest {
+
+        override def setupStubs(): StubMapping = {
+          AuditStub.audit()
+          AuthStub.authorised()
+          MtdIdLookupStub.ninoFound(nino)
+          DesStub.serviceSuccess(nino)
+        }
+
+        val response: WSResponse = await(request().post(requestBody))
+        response.status shouldBe Status.NOT_FOUND
+        response.json shouldBe Json.toJson(NotFoundError)
+      }
+    }
+
+    "return 500 (Internal Server Error)" when {
+
+      retrieveObligationsErrorTest(Status.BAD_REQUEST, "INVALID_PAYLOAD", Status.INTERNAL_SERVER_ERROR, DownstreamError)
+      retrieveObligationsErrorTest(Status.SERVICE_UNAVAILABLE, "SERVICE_UNAVAILABLE", Status.INTERNAL_SERVER_ERROR, DownstreamError)
+      retrieveObligationsErrorTest(Status.INTERNAL_SERVER_ERROR, "SERVER_ERROR", Status.INTERNAL_SERVER_ERROR, DownstreamError)
+    }
+
+    "return 400 (Bad Request)" when {
+      retrieveObligationsErrorTest(Status.BAD_REQUEST, "INVALID_IDNUMBER", Status.BAD_REQUEST, NinoFormatError)
+      retrieveObligationsErrorTest(Status.BAD_REQUEST, "INVALID_DATE_TO", Status.BAD_REQUEST, TaxYearFormatError)
+      retrieveObligationsErrorTest(Status.BAD_REQUEST, "INVALID_DATE_FROM", Status.BAD_REQUEST, RuleIncorrectOrEmptyBodyError)
+      retrieveObligationsErrorTest(Status.BAD_REQUEST, "INVALID_DATE_FROM", Status.BAD_REQUEST, RuleTaxYearNotSupportedError)
+      retrieveObligationsErrorTest(Status.BAD_REQUEST, "INVALID_DATE_FROM", Status.BAD_REQUEST, RuleTaxYearRangeExceededError)
+      retrieveObligationsErrorTest(Status.BAD_REQUEST, "INVALID_DATE_FROM", Status.BAD_REQUEST, RuleTypeOfLossUnsupported)
+      retrieveObligationsErrorTest(Status.BAD_REQUEST, "INVALID_DATE_FROM", Status.BAD_REQUEST, RuleInvalidSelfEmploymentId)
+      retrieveObligationsErrorTest(Status.BAD_REQUEST, "INVALID_DATE_FROM", Status.BAD_REQUEST, RulePropertySelfEmploymentId)
+      retrieveObligationsErrorTest(Status.BAD_REQUEST, "INVALID_DATE_FROM", Status.BAD_REQUEST, AmountFormatError)
+      retrieveObligationsErrorTest(Status.BAD_REQUEST, "INVALID_DATE_FROM", Status.BAD_REQUEST, RuleInvalidLossAmount)
+
+      BadRequestError
+      | NinoFormatError
+        | TaxYearFormatError
+        | RuleIncorrectOrEmptyBodyError
+        | RuleTaxYearNotSupportedError
+        | RuleTaxYearRangeExceededError
+        | RuleTypeOfLossUnsupported
+        | RuleInvalidSelfEmploymentId
+        | RulePropertySelfEmploymentId
+        | AmountFormatError
+        | RuleInvalidLossAmount
+    }
+
+    "return 404 NOT FOUND" when {
+      retrieveObligationsErrorTest(Status.NOT_FOUND, "NOT_FOUND", Status.NOT_FOUND, NotFoundError)
+    }
+  }*/
   }
 }
