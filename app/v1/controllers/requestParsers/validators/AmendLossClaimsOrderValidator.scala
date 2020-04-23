e01b8bc6f693033a840de7ce731d2d1fd6749479
Mbrookesy <56401421+Mbrookesy@users.noreply.github.com>
2020-04-23 11:49:54 +0100

    Skeleton for Request Parser and Validator added

---
+++ app/v1/controllers/requestParsers/validators/AmendLossClaimsOrderValidator.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.controllers.requestParsers.validators
+
+import v1.controllers.requestParsers.validators.validations.{ClaimIdValidation, ClaimTypeValidation, JsonFormatValidation, JsonValidation, NinoValidation, TaxYearValidation}
+import v1.models.errors.{MtdError, RuleIncorrectOrEmptyBodyError}
+
+class AmendLossClaimsOrderValidator extends Validator[???] {
+
+  val validationSet = List(paramaterFormatValidation, bodyFormatValidation, bodyFieldValidation)
+
+  private def paramaterFormatValidation: ??? => List[List[MtdError]] = { data =>
+    List(
+      NinoValidation.validate(???),
+      TaxYearValidation.validate(???)
+    )
+  }
+
+  private def bodyFormatValidation: ??? => List[List[MtdError]] = { data =>
+    List(
+      JsonFormatValidation.validate[???](???, RuleIncorrectOrEmptyBodyError)
+    )
+  }
+
+  private def bodyFieldValidation: ??? => List[List[MtdError]] = { data =>
+    val req = ???
+    List(
+      JsonValidation.validate[String](??? \ "claimType")(ClaimTypeValidation.validate),
+      JsonValidation.validate[String](??? \ "listOfLossClaims" \ "id")(ClaimIdValidation.validate),
+      JsonValidation.validate[String](??? \ "listOfLossClaims" \ "sequence")(???),
+    )
+  }
+
+
+
+}
