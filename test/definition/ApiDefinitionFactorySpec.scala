0a8c14310b97e12bf5a07a19da69178e203c2fc9
RachGibbons <rach.gibbons@digital.hmrc.gov.uk>
2021-06-11 08:07:25 +0100

    MTDSA-9158 added whitelistingaccess into the code

--- test/definition/ApiDefinitionFactorySpec.scala
+++ test/definition/ApiDefinitionFactorySpec.scala
@@ -16,11 +16,13 @@
 
 package definition
 
+import com.typesafe.config.ConfigFactory
 import config.ConfidenceLevelConfig
 import definition.APIStatus.{ALPHA, BETA}
 import mocks.MockAppConfig
 import support.UnitSpec
 import uk.gov.hmrc.auth.core.ConfidenceLevel
+import play.api.Configuration
 
 class ApiDefinitionFactorySpec extends UnitSpec with MockAppConfig {
 
@@ -100,6 +102,35 @@ class ApiDefinitionFactorySpec extends UnitSpec with MockAppConfig {
       "default to alpha" in new Test {
         MockAppConfig.apiStatus(status = "1.0") returns "ALPHO"
         factory.buildAPIStatus(version = "1.0") shouldBe ALPHA
+      }
+    }
+  }
+
+  "buildWhiteListingAccess" when {
+    "the 'featureSwitch' parameter is not present" should {
+      "return None" in new Test {
+        MockAppConfig.featureSwitch returns None
+      }
+    }
+
+    "the 'featureSwitch' parameter is present and white listing is enabled" should {
+      "return the correct Access object" in new Test {
+
+        private val someString =
+          """
+            |{
+            |   white-list.enabled = true
+            |   white-list.applicationIds = ["anId"]
+            |}
+          """.stripMargin
+
+        MockAppConfig.featureSwitch returns Some(Configuration(ConfigFactory.parseString(someString)))
+      }
+    }
+
+    "the 'featureSwitch' parameter is present and white listing is not enabled" should {
+      "return None" in new Test {
+        MockAppConfig.featureSwitch returns Some(Configuration(ConfigFactory.parseString("""white-list.enabled = false""")))
       }
     }
   }
