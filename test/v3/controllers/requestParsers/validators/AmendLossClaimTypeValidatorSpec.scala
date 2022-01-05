a1d69b7715edced6ecaf20dbc344682bdb50ad06
jeremystone <6453521+jeremystone@users.noreply.github.com>
2022-01-05 11:40:30 +0000

    MTDSA-10121 - amend a loss claim type validator and parser

--- test/v3/controllers/requestParsers/validators/AmendLossClaimTypeValidatorSpec.scala
+++ test/v3/controllers/requestParsers/validators/AmendLossClaimTypeValidatorSpec.scala
@@ -16,22 +16,18 @@
 
 package v3.controllers.requestParsers.validators
 
-import play.api.libs.json.Json
+import play.api.libs.json.{ JsObject, Json }
 import play.api.mvc.AnyContentAsJson
 import support.UnitSpec
-import v3.models.errors.{ClaimIdFormatError, NinoFormatError, RuleIncorrectOrEmptyBodyError, TypeOfClaimFormatError}
+import v3.models.errors.{ ClaimIdFormatError, NinoFormatError, RuleIncorrectOrEmptyBodyError, TypeOfClaimFormatError }
 import v3.models.requestData.AmendLossClaimTypeRawData
-
-import scala.collection.immutable.Stream.Empty
 
 class AmendLossClaimTypeValidatorSpec extends UnitSpec {
 
-  private val validNino = "AA123456A"
-  private val invalidNino = "AA123456"
-  private val validClaimId = "AAZZ1234567890a"
-  private val invalidClaimId = "AAZZ1234567890"
+  private val validNino      = "AA123456A"
+  private val validClaimId   = "AAZZ1234567890a"
 
-  private def claimType(claimType: String) = AnyContentAsJson(Json.obj("typeOfClaim" -> claimType))
+  private def requestBodyJson(claimType: String = "carry-forward") = AnyContentAsJson(Json.obj("typeOfClaim" -> claimType))
 
   val validator = new AmendLossClaimTypeValidator
 
@@ -40,26 +36,37 @@ class AmendLossClaimTypeValidatorSpec extends UnitSpec {
     "return no errors" when {
 
       "all data is valid" in {
-        validator.validate(AmendLossClaimTypeRawData(validNino, validClaimId, claimType("carry-forward"))) shouldBe Empty
+        validator.validate(AmendLossClaimTypeRawData(validNino, validClaimId, requestBodyJson())) shouldBe Nil
       }
     }
 
-    "return one error" when {
-
+    "return NinoFormatError" when {
       "nino validation fails" in {
-        validator.validate(AmendLossClaimTypeRawData(invalidNino, validClaimId, claimType("carry-forward"))) shouldBe List(NinoFormatError)
+        validator.validate(AmendLossClaimTypeRawData("invalid", validClaimId, requestBodyJson())) shouldBe List(NinoFormatError)
       }
+    }
 
+    "return ClaimIdFormatError" when {
       "claimId validation fails" in {
-        validator.validate(AmendLossClaimTypeRawData(validNino, invalidClaimId, claimType("carry-forward"))) shouldBe List(ClaimIdFormatError)
+        validator.validate(AmendLossClaimTypeRawData(validNino, "invalid", requestBodyJson())) shouldBe List(ClaimIdFormatError)
+      }
+    }
+
+    "return RuleIncorrectOrEmptyBodyError" when {
+      "an empty body is supplied" in {
+        validator.validate(AmendLossClaimTypeRawData(validNino, validClaimId, AnyContentAsJson(JsObject.empty))) shouldBe List(
+          RuleIncorrectOrEmptyBodyError)
       }
 
-      "body validation fails" in {
-        validator.validate(AmendLossClaimTypeRawData(validNino, validClaimId, AnyContentAsJson(Json.obj()))) shouldBe List(RuleIncorrectOrEmptyBodyError)
+      "an incorrect body is supplied" in {
+        validator.validate(AmendLossClaimTypeRawData(validNino, validClaimId, AnyContentAsJson(Json.obj("claimType" -> "carry-forward")))) shouldBe List(
+          RuleIncorrectOrEmptyBodyError.copy(paths = Some(Seq("/typeOfClaim"))))
       }
+    }
 
+    "return TypeOfClaimFormatError" when {
       "type of claim validation fails" in {
-        validator.validate(AmendLossClaimTypeRawData(validNino, validClaimId, claimType("invalid"))) shouldBe List(TypeOfClaimFormatError)
+        validator.validate(AmendLossClaimTypeRawData(validNino, validClaimId, requestBodyJson("invalid"))) shouldBe List(TypeOfClaimFormatError)
       }
     }
   }
