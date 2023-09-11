d4cde115148cb4afeddfe0ffa31cdf9e8732f2ee
chetan-jain-atos <112093269+chetan-jain-atos@users.noreply.github.com>
2023-09-11 17:09:03 +0100

    MTDSA-19037: prepare API for validator improvements

--- app/api/controllers/RequestHandler.scala
+++ app/api/controllers/RequestHandler.scala
@@ -34,6 +34,7 @@ import scala.concurrent.{ExecutionContext, Future}
 trait RequestHandler[InputRaw <: RawData] {
 
   def handleRequest(rawData: InputRaw)(implicit ctx: RequestContext, request: UserRequest[_], ec: ExecutionContext): Future[Result]
+
 }
 
 object RequestHandler {
