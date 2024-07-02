04c1765c39ee7b68adb7e41a9088ceca5e91c381
Vedant-N421 <147396311+Vedant-N421@users.noreply.github.com>
2024-07-02 13:49:44 +0100

    MTDSA-23459: Completed the createBF endpoint changes

---
+++ app/v5/bfLossClaims/create/def1/model/request/Def1_CreateBFLossRequestData.scala
@@ -0,0 +1,10 @@
+
+package v5.bfLossClaims.create.def1.model.request
+
+import api.models.domain.Nino
+import v5.bfLossClaims.create.CreateBFLossSchema
+import v5.bfLossClaims.create.model.request.CreateBFLossRequestData
+
+case class Def1_CreateBFLossRequestData (nino: Nino,broughtForwardLoss: Def1_CreateBFLossRequestBody) extends CreateBFLossRequestData {
+  val schema: CreateBFLossSchema = CreateBFLossSchema.Def1
+}
