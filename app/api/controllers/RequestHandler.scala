9c37d64d747baf8a66359917091c6a602afd1c46
MadStu <737342+MadStu@users.noreply.github.com>
2024-08-20 13:34:20 +0100

    [MTDSA-25316] Remove the X-Content-Type-Options header

--- app/api/controllers/RequestHandler.scala
+++ app/api/controllers/RequestHandler.scala
@@ -135,8 +135,7 @@ object RequestHandler {
           val headers =
             responseHeaders ++
               List(
-                "X-CorrelationId"        -> correlationId,
-                "X-Content-Type-Options" -> "nosniff"
+                "X-CorrelationId" -> correlationId
               ) ++
               maybeDeprecatedHeader
 
