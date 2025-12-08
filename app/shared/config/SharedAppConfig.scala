745cd5bc24afd7a97a393fc4196a2b6e9e19eeae
stephen-oppong-beduh <56881842+stephen-oppong-beduh@users.noreply.github.com>
2025-12-08 16:18:27 +0000

    MTDSA-32312 - Expose Default Sunset Date when not configured

--- app/shared/config/SharedAppConfig.scala
+++ app/shared/config/SharedAppConfig.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 HM Revenue & Customs
+ * Copyright 2025 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -118,7 +118,7 @@ class SharedAppConfig @Inject() (val config: ServicesConfig, protected[config] v
             Deprecated(dO, Some(sD)).valid
           else
             s"sunsetDate must be later than deprecatedOn date for a deprecated version $version".invalid
-        case (Some(dO), None, true) => Deprecated(dO, Some(dO.plusMonths(6).plusDays(1))).valid
+        case (Some(dO), None, true) => Deprecated(dO, Some(dO.plusMonths(6))).valid
         case (Some(dO), _, false)   => Deprecated(dO, None).valid
         case _                      => s"deprecatedOn date is required for a deprecated version $version".invalid
       }
--- app/shared/controllers/RequestHandler.scala
+++ app/shared/controllers/RequestHandler.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 HM Revenue & Customs
+ * Copyright 2025 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -121,17 +121,11 @@ object RequestHandler {
         private def withDeprecationHeaders: List[(String, String)] = {
 
           appConfig.deprecationFor(apiVersion) match {
-            case Valid(Deprecated(deprecatedOn, Some(sunsetDate))) =>
-              List(
-                "Deprecation" -> longDateTimestampGmt(deprecatedOn),
-                "Sunset"      -> longDateTimestampGmt(sunsetDate),
-                "Link"        -> appConfig.apiDocumentationUrl
-              )
-            case Valid(Deprecated(deprecatedOn, None)) =>
+            case Valid(Deprecated(deprecatedOn, maybeSunsetDate)) =>
               List(
                 "Deprecation" -> longDateTimestampGmt(deprecatedOn),
                 "Link"        -> appConfig.apiDocumentationUrl
-              )
+              ) ++ maybeSunsetDate.map(sunsetDate => "Sunset" -> longDateTimestampGmt(sunsetDate))
             case _ => Nil
           }
         }
