0aec884869113168d2c79f2c7e0f1eec8c3417da
muhammadmulla1 <muhammad_mulla@icloud.com>
2025-12-22 13:54:02 +0000

    MTDSA-32417 - Return 504 GATEWAY_TIMEOUT for 499s and 504s from downstream

--- test/shared/utils/ErrorHandlerSpec.scala
+++ test/shared/utils/ErrorHandlerSpec.scala
@@ -25,7 +25,7 @@ import play.api.test.FakeRequest
 import play.api.test.Helpers.*
 import shared.models.errors.*
 import uk.gov.hmrc.auth.core.InsufficientEnrolments
-import uk.gov.hmrc.http.{HeaderCarrier, JsValidationException, NotFoundException}
+import uk.gov.hmrc.http.{HeaderCarrier, JsValidationException, NotFoundException, UpstreamErrorResponse}
 import uk.gov.hmrc.play.audit.http.connector.AuditConnector
 import uk.gov.hmrc.play.audit.http.connector.AuditResult.Success
 import uk.gov.hmrc.play.audit.model.{DataEvent, TruncationLog}
@@ -139,6 +139,18 @@ class ErrorHandlerSpec extends UnitSpec with GuiceOneAppPerSuite {
         status(result) shouldBe INTERNAL_SERVER_ERROR
 
         contentAsJson(result) shouldBe InternalError.asJson
+      }
+    }
+
+    "return GATEWAY_TIMEOUT with error body" when {
+      Seq(499, GATEWAY_TIMEOUT).foreach { statusCode =>
+        s"a $statusCode UpstreamErrorResponse is returned" in new Test {
+          val errorResponse: UpstreamErrorResponse = UpstreamErrorResponse("request timeout", statusCode, statusCode, Map.empty)
+          val result: Future[Result]               = handler.onServerError(requestHeader, errorResponse)
+
+          status(result) shouldBe GATEWAY_TIMEOUT
+          contentAsJson(result) shouldBe GatewayTimeoutError.asJson
+        }
       }
     }
   }
