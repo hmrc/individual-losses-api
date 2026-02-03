b03feffba0df28905a10814e425eee4098b18ea0
TwinkleLahariyaHMRC <211375592+TwinkleLahariyaHMRC@users.noreply.github.com>
2026-02-03 12:54:26 +0000

    [MTDSA-32809]: Integration test fix for Create Amend Losses and claims

--- it/test/v7/lossesAndClaims/CreateAmendLossesAndClaimsControllerISpec.scala
+++ it/test/v7/lossesAndClaims/CreateAmendLossesAndClaimsControllerISpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2027 HM Revenue & Customs
+ * Copyright 2025 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,8 @@ import com.github.tomakehurst.wiremock.stubbing.StubMapping
 import common.errors.RuleOutsideAmendmentWindow
 import play.api.http.HeaderNames.ACCEPT
 import play.api.http.Status.*
-import play.api.libs.json.{JsObject, JsValue, Json}
+import play.api.libs.json.{JsValue, Json}
+import play.api.libs.ws.WSBodyWritables.writeableOf_JsValue
 import play.api.libs.ws.{WSRequest, WSResponse}
 import play.api.test.Helpers.AUTHORIZATION
 import shared.models.errors.*
@@ -29,187 +30,144 @@ import shared.support.IntegrationBaseSpec
 
 class CreateAmendLossesAndClaimsControllerISpec extends IntegrationBaseSpec {
 
-  private def errorBody(code: String): String =
-    s"""
-       |{
-       |  "response": [
-       |    {
-       |      "errorCode": "$code",
-       |      "errorDescription": "message"
-       |    }
-       |  ]
-       |}
-      """.stripMargin
-
-  val mtdRequestBody: JsValue = Json.parse(s"""
-       |{
-       |  "claims": {
-       |    "carryBack": {
-       |      "previousYearGeneralIncome": 5000.99,
-       |      "earlyYearLosses": 5000.99
-       |    },
-       |    "carrySideways": {
-       |      "currentYearGeneralIncome": 5000.99
-       |    },
-       |    "preferenceOrder": {
-       |      "applyFirst": "carry-sideways"
-       |    },
-       |    "carryForward": {
-       |      "currentYearLosses": 5000.99,
-       |      "previousYearsLosses": 5000.99
-       |    }
-       |  },
-       |  "losses": {
-       |    "broughtForwardLosses": 5000.99
-       |  }
-       |}
-      """.stripMargin)
-
-  val downstreamRequestBodyJson: JsValue = Json.parse(s"""
-       |{
-       |  "claims": {
-       |    "carryBack": {
-       |      "previousYearGeneralIncomeSection64": 5000.99,
-       |      "earlyYearLossesSection72": 5000.99
-       |    },
-       |    "carrySideways": {
-       |      "currentYearGeneralIncomeSection64": 5000.99
-       |    },
-       |    "preferenceOrderSection64": {
-       |      "applyFirst": "carry-sideways"
-       |    },
-       |    "carryForward": {
-       |      "currentYearLossesSection83": 5000.99,
-       |      "previousYearsLossesSection83": 5000.99
-       |    }
-       |  },
-       |  "losses": {
-       |    "broughtForwardLosses": 5000.99
-       |  }
-       |}
-       |""".stripMargin)
-
-  "Calling the Create Amend losses and claims endpoint" should {
-    "return a 204 status code" when {
-      "any valid request is made" in new Test {
-
-        override def setupStubs(): StubMapping = {
-          AuditStub.audit()
-          AuthStub.authorised()
-          MtdIdLookupStub.ninoFound(nino)
-          DownstreamStub
-            .when(DownstreamStub.PUT, downstreamUri, downstreamQueryParam)
-            .withRequestBody(mtdRequestBody)
-            .thenReturn(NO_CONTENT, JsObject.empty)
-        }
-
-        val response: WSResponse = await(request().put(downstreamRequestBodyJson))
-        response.status shouldBe NO_CONTENT
-        response.body shouldBe ""
-      }
-    }
-
-    "return error according to spec" when {
-      "validation error" when {
-        "validation error" when {
-          def validationErrorTest(requestNino: String,
-                                  requestBusinessId: String,
-                                  requestTaxYear: String,
-                                  expectedStatus: Int,
-                                  expectedBody: MtdError): Unit = {
-            s"validation fails with ${expectedBody.code} error" in new Test {
-
-              override val nino: String       = requestNino
-              override val businessId: String = requestBusinessId
-              override val taxYear: String    = requestTaxYear
-
-              override def setupStubs(): StubMapping = {
-                AuditStub.audit()
-                MtdIdLookupStub.ninoFound(nino)
-                AuthStub.authorised()
-                DownstreamStub
-                  .when(DownstreamStub.PUT, downstreamUri, downstreamQueryParam)
-                  .withRequestBody(downstreamRequestBodyJson)
-                  .thenReturn(NO_CONTENT, JsObject.empty)
-              }
-
-              val response: WSResponse = await(request().put(mtdRequestBody))
-              response.status shouldBe expectedStatus
-            }
-          }
-
-          val input = List(
-            ("AA1123A", "XAIS12345678910", "2026-27", BAD_REQUEST, NinoFormatError),
-            ("AA123456A", "invalid", "2026-27", BAD_REQUEST, BusinessIdFormatError),
-            ("AA123456A", "XAIS12345678910", "invalid", BAD_REQUEST, TaxYearFormatError),
-            ("AA123456A", "XAIS12345678910", "2025-27", BAD_REQUEST, RuleTaxYearRangeInvalidError),
-            ("AA123456A", "XAIS12345678910", "2025-26", BAD_REQUEST, RuleTaxYearNotSupportedError)
-          )
-
-          input.foreach(validationErrorTest.tupled)
-        }
-      }
-
-      "downstream service error" when {
-        def serviceErrorTest(downstreamStatus: Int, downstreamCode: String, expectedStatus: Int, expectedBody: MtdError): Unit = {
-          s"downstream returns an $downstreamCode error and status $downstreamStatus" in new Test {
-            override def setupStubs(): StubMapping = {
-              AuditStub.audit()
-              MtdIdLookupStub.ninoFound(nino)
-              AuthStub.authorised()
-              DownstreamStub.onError(DownstreamStub.PUT, downstreamUri, downstreamStatus, errorBody(downstreamCode))
-            }
-
-            val response: WSResponse = await(request().put(mtdRequestBody))
-            response.json shouldBe Json.toJson(expectedBody)
-            response.status shouldBe expectedStatus
-          }
-        }
-
-        val errors = List(
-          (BAD_REQUEST, "1215", BAD_REQUEST, NinoFormatError),
-          (BAD_REQUEST, "1117", BAD_REQUEST, TaxYearFormatError),
-          (BAD_REQUEST, "1216", INTERNAL_SERVER_ERROR, InternalError),
-          (BAD_REQUEST, "1000", INTERNAL_SERVER_ERROR, InternalError),
-          (BAD_REQUEST, "1007", BAD_REQUEST, BusinessIdFormatError),
-          (UNPROCESSABLE_ENTITY, "1115", BAD_REQUEST, RuleTaxYearNotEndedError),
-          (UNPROCESSABLE_ENTITY, "1253", BAD_REQUEST, RuleMissingPreferenceOrder),
-          (UNPROCESSABLE_ENTITY, "1254", BAD_REQUEST, RuleCarryForwardAndTerminalLossNotAllowed),
-          (UNPROCESSABLE_ENTITY, "1262", BAD_REQUEST, RuleCarryBackClaim),
-          (UNPROCESSABLE_ENTITY, "4200", BAD_REQUEST, RuleOutsideAmendmentWindow),
-          (NOT_IMPLEMENTED, "5000", InternalError, RuleTaxYearNotSupportedError),
-          (NOT_FOUND, "5010", NOT_FOUND, NotFoundError),
-          (BAD_REQUEST, "UNMATCHED_STUB_ERROR", BAD_REQUEST, RuleIncorrectGovTestScenarioError)
-        )
-
-        errors.foreach(serviceErrorTest.tupled)
-      }
-    }
-  }
+  val typeOfClaim = "carry-forward"
+  val typeOfLoss  = "self-employment"
+  val claimId     = "AAZZ1234567890a"
 
   private trait Test {
 
-    val nino: String       = "AA123456A"
-    val businessId: String = "X0IS12345678901"
-    val taxYear: String    = "2026-27"
+    val nino       = "AA123456A"
+    val businessId = "XKIS00000000988"
+    val taxYear    = "2026-27"
 
-    val downstreamQueryParam: Map[String, String] = Map("taxYear" -> "26-27")
+    val requestJson: JsValue = Json.parse(s"""
+         |{
+         |  "claims": {
+         |    "carryBack": {
+         |      "previousYearGeneralIncome": 5000.99,
+         |      "earlyYearLosses": 5000.99
+         |    },
+         |    "carrySideways": {
+         |      "currentYearGeneralIncome": 5000.99
+         |    },
+         |    "preferenceOrder": {
+         |      "applyFirst": "carry-sideways"
+         |    },
+         |    "carryForward": {
+         |      "currentYearLosses": 5000.99,
+         |      "previousYearsLosses": 5000.99
+         |    }
+         |  },
+         |  "losses": {
+         |    "broughtForwardLosses": 5000.99
+         |  }
+         |}
+      """.stripMargin)
 
-    def downstreamUri: String = s"/itsd/reliefs/loss-claims/$nino/$businessId"
+    val downstreamRequestJson: JsValue = Json.parse(s"""
+         |{
+         |  "claims": {
+         |    "carryBack": {
+         |      "previousYearGeneralIncomeSection64": 5000.99,
+         |      "earlyYearLossesSection72": 5000.99
+         |    },
+         |    "carrySideways": {
+         |      "currentYearGeneralIncomeSection64": 5000.99
+         |    },
+         |    "preferenceOrderSection64": {
+         |      "applyFirst": "carry-sideways"
+         |    },
+         |    "carryForward": {
+         |      "currentYearLossesSection83": 5000.99,
+         |      "previousYearsLossesSection83": 5000.99
+         |    }
+         |  },
+         |  "losses": {
+         |    "broughtForwardLosses": 5000.99
+         |  }
+         |}
+      """.stripMargin)
 
-    private def mtdUri: String = s"/$nino/businesses/$businessId/loss-claims/$taxYear"
+    def errorBody(code: String): String =
+      s"""
+         |[
+         |  {
+         |    "errorCode": "$code",
+         |    "errorDescription": "string"
+         |  }
+         |]
+      """.stripMargin
+
+    def setupStubs(): StubMapping
+
+    def uri: String
 
     def request(): WSRequest = {
-      AuthStub.resetAll()
       setupStubs()
-
-      buildRequest(mtdUri)
+      buildRequest(uri)
         .withHttpHeaders(
           (ACCEPT, "application/vnd.hmrc.7.0+json"),
           (AUTHORIZATION, "Bearer 123")
         )
     }
 
+  }
+
+  "Calling the create amend losses and claims endpoint" should {
+
+    trait CreateAmendLossesAnsClaimsControllerTest extends Test {
+      def uri: String = s"/$nino/businesses/$businessId/loss-claims/$taxYear"
+
+      def downstreamURL: String = s"/itsd/reliefs/loss-claims/$nino/$businessId"
+    }
+
+    "return a 204 status code" when {
+      "any valid request is made" in new CreateAmendLossesAnsClaimsControllerTest() {
+
+        override def setupStubs(): StubMapping = {
+          AuditStub.audit()
+          AuthStub.authorised()
+          MtdIdLookupStub.ninoFound(nino)
+          DownstreamStub.onSuccess(DownstreamStub.PUT, downstreamURL, NO_CONTENT)
+        }
+
+        val response: WSResponse = await(request().post(requestJson))
+        response.status shouldBe NO_CONTENT
+      }
+    }
+
+    "downstream service error" when {
+
+      createErrorTest(BAD_REQUEST, "1215", BAD_REQUEST, NinoFormatError)
+      createErrorTest(BAD_REQUEST, "1117", BAD_REQUEST, TaxYearFormatError)
+      createErrorTest(BAD_REQUEST, "1216", INTERNAL_SERVER_ERROR, InternalError)
+      createErrorTest(BAD_REQUEST, "1000", INTERNAL_SERVER_ERROR, InternalError)
+      createErrorTest(BAD_REQUEST, "1007", BAD_REQUEST, BusinessIdFormatError)
+      createErrorTest(UNPROCESSABLE_ENTITY, "1115", BAD_REQUEST, RuleTaxYearNotEndedError)
+      createErrorTest(UNPROCESSABLE_ENTITY, "1253", BAD_REQUEST, RuleMissingPreferenceOrder)
+      createErrorTest(UNPROCESSABLE_ENTITY, "1254", BAD_REQUEST, RuleCarryForwardAndTerminalLossNotAllowed)
+      createErrorTest(UNPROCESSABLE_ENTITY, "1262", BAD_REQUEST, RuleCarryBackClaim)
+      createErrorTest(UNPROCESSABLE_ENTITY, "4200", BAD_REQUEST, RuleOutsideAmendmentWindow)
+      createErrorTest(NOT_IMPLEMENTED, "5000", BAD_REQUEST, RuleTaxYearNotSupportedError)
+      createErrorTest(NOT_FOUND, "5010", NOT_FOUND, NotFoundError)
+      createErrorTest(BAD_REQUEST, "UNMATCHED_STUB_ERROR", BAD_REQUEST, RuleIncorrectGovTestScenarioError)
+    }
+
+    def createErrorTest(status: Int, code: String, expectedStatus: Int, expectedBody: MtdError): Unit = {
+      s"downstream returns an $code error" in new CreateAmendLossesAnsClaimsControllerTest {
+
+        override def setupStubs(): StubMapping = {
+          AuditStub.audit()
+          AuthStub.authorised()
+          MtdIdLookupStub.ninoFound(nino)
+          DownstreamStub.onError(DownstreamStub.PUT, downstreamURL, status, errorBody(code))
+        }
+
+        val response: WSResponse = await(request().post(requestJson))
+        response.status shouldBe expectedStatus
+        response.json shouldBe Json.toJson(expectedBody)
+      }
+    }
   }
 
 }
