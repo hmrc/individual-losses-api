44ae553e997ddf603af7b8df9422bcb74fea9a56
muhammadmulla1 <muhammad_mulla@icloud.com>
2026-06-19 14:27:26 +0100

    MTDSA-35101: Replace CORS configuration with CORSActionBuilder to resolve documentation rendering issues in Individual Losses API

--- app/api/controllers/DocumentationController.scala
+++ app/api/controllers/DocumentationController.scala
@@ -19,28 +19,38 @@ package api.controllers
 import api.config.rewriters.DocumentationRewriters
 import api.definition.ApiDefinitionFactory
 import controllers.RewriteableAssets
+import org.apache.pekko.stream.Materializer
+import play.api.Configuration
 import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import play.filters.cors.CORSActionBuilder
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 
 import javax.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext
 
 @Singleton
 class DocumentationController @Inject() (
     selfAssessmentApiDefinition: ApiDefinitionFactory,
     docRewriters: DocumentationRewriters,
     assets: RewriteableAssets,
+    configuration: Configuration,
     cc: ControllerComponents
-) extends BackendController(cc) {
+)(implicit ec: ExecutionContext, materializer: Materializer)
+    extends BackendController(cc) {
 
   def definition(): Action[AnyContent] = Action {
     Ok(Json.toJson(selfAssessmentApiDefinition.definition))
   }
 
   def asset(version: String, filename: String): Action[AnyContent] = {
-    val path      = s"/public/api/conf/$version"
-    val rewriters = docRewriters.rewriteables.flatMap { _.maybeRewriter(version, filename) }
-    assets.rewriteableAt(path, filename, rewriters)
+    CORSActionBuilder(configuration).async { implicit request =>
+      val path = s"/public/api/conf/$version"
+      val rewriters = docRewriters.rewriteables.flatMap {
+        _.maybeRewriter(version, filename)
+      }
+      assets.rewriteableAt(path, filename, rewriters)(request)
+    }
   }
 
 }
--- test/api/controllers/DocumentationControllerSpec.scala
+++ test/api/controllers/DocumentationControllerSpec.scala
@@ -23,6 +23,8 @@ import api.definition.*
 import api.routing.{Version, Versions}
 import com.typesafe.config.ConfigFactory
 import controllers.{AssetsConfiguration, DefaultAssetsMetadata, RewriteableAssets}
+import org.apache.pekko.stream.Materializer
+import org.apache.pekko.stream.testkit.NoMaterializer
 import play.api.http.{DefaultFileMimeTypes, DefaultHttpErrorHandler, FileMimeTypesConfiguration, HttpConfiguration}
 import play.api.mvc.Result
 import play.api.{Configuration, Environment}
@@ -211,8 +213,9 @@ class DocumentationControllerSpec extends ControllerBaseSpec with MockAppConfig 
       new OasFeatureRewriter()(mockAppConfig)
     )
 
-    private val assets       = new RewriteableAssets(errorHandler, assetsMetadata, mock[Environment])
-    protected def controller = new DocumentationController(apiFactory, docRewriters, assets, cc)
+    private val assets                              = new RewriteableAssets(errorHandler, assetsMetadata, mock[Environment])
+    implicit private val materializer: Materializer = NoMaterializer
+    protected def controller                        = new DocumentationController(apiFactory, docRewriters, assets, config, cc)
   }
 
 }
